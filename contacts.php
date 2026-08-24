a6428124208139472bd183247aaf1fd3ad22117c
Yterror <138582052+Yterror@users.noreply.github.com>
2026-08-24 15:34:10 -0400

    Contacts
    Добавлена страница контакты

---
+++ contacts.php
@@ -0,0 +1,93 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Контакты</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+<header>
+
+    <div class="container">
+
+        <div class="logo">
+
+            <a href="index.php">
+                <span>МУИВ</span>
+                <small>Web-портал опросов</small>
+            </a>
+
+        </div>
+
+        <nav>
+
+            <ul class="menu">
+
+                <li><a href="index.php">Главная</a></li>
+
+            </ul>
+
+        </nav>
+
+    </div>
+
+</header>
+
+<main>
+
+<section class="about">
+
+    <h1>Контакты</h1>
+
+    <p>
+        По вопросам работы Web-портала пользователи могут
+        обратиться в университет.
+    </p>
+
+    <h2>Контактная информация</h2>
+
+    <p>
+        Московский университет имени С.Ю. Витте
+    </p>
+
+    <p>
+        Адрес: Москва, 2-й Кожуховский проезд, дом 12, строение 1.
+    </p>
+
+    <p>
+        Электронная почта: info@muiv.ru
+    </p>
+
+</section>
+
+</main>
+
+<footer>
+
+© 2026 Московский университет им. С.Ю. Витте
+
+</footer>
+
+</body>
+
+</html>
